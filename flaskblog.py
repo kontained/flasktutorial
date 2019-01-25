337bd9454e81c166e29bfd248a4062b54ccc35fe
= <kontained@gmail.com>
2019-01-25 12:00:09 -0600

    Added templates folder, moving html to templates.

--- flaskblog.py
+++ flaskblog.py
@@ -1,13 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return '<h1>Home Page</h1>'
+    return render_template('home')
 
 
 @app.route('/about')
 def about():
-    return '<h1>About Page</h2>'
+    return render_template('about')
