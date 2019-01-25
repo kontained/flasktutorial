f94f33101a6fdeb18292d598aa3590b403b89186
= <kontained@gmail.com>
2019-01-25 15:34:02 -0600

    Renaming template names so that they will actually work..

--- application.py
+++ application.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home')
+    return render_template('home.html')
 
 
 @app.route('/about')
 def about():
-    return render_template('about')
+    return render_template('about.html')
